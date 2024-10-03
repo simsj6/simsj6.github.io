f49e3ecdf756f24ea171b8faa1a95e097bcbc0a1
Jenny Sims <simsj6@wwu.edu>
2024-10-03 16:14:10 -0700

    Added responsive hamburger nav

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
