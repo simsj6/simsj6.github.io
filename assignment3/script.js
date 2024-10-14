7f6255daeacddc8142bf4a9e27cf07f57a850ad2
Jenny Sims <simsj6@wwu.edu>
2024-10-14 11:48:56 -0700

    Started assignment 3 javascript

---
+++ assignment3/script.js
@@ -0,0 +1,16 @@
+// hamburger menu on small screens
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
