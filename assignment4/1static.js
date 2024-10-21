d8c91689346b24ff6d879f0319bafbb136f60bf1
Jenny Sims <simsj6@wwu.edu>
2024-10-21 11:46:44 -0700

    Starting circle colors

--- assignment4/1static.js
+++ assignment4/1static.js
@@ -1,12 +1,179 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-}
-
-function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+    createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background("gray");
-  circle(100, 200, 100);
+    background(46, 48, 64);
+
+    // 1
+    fill(13, 33, 191);
+    circle((width / 2.67), (height / 3), height / 3);
+
+    // 2
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 3
+    fill(251,230,85);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 4
+    fill(249,153,216);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 5
+    fill(192,187,219);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 6
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 7
+    fill(203,140,221);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 8
+    fill(250,226,130);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 9
+    fill(255,174,30);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 10
+    fill(241,63,87);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 11
+    fill(72,133,126);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 12
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 13
+    fill(107,191,219); // not sure here
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 14
+    fill(161,221,169);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 15
+    fill(255,165,53);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 16
+    fill(205,174,49);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 17
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 18
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 19
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 20
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 21
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 22
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 23
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 24
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 25
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 26
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 27
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 28
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 29
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 30
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 31
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 32
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 33
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 34
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 35
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 36
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 37
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 38
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 39
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 40
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 41
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 42
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
+
+    // 43
+    fill(0,0,0);
+    circle((width / 3), (height / 3.55), height / 5);
 }
