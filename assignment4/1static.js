a9c98bbdbbfbb0a8ebaea2c98dec479649b7229b
Jenny Sims <simsj6@wwu.edu>
2024-10-23 20:19:05 -0700

    Saving before I start changing widths

--- assignment4/1static.js
+++ assignment4/1static.js
@@ -4,176 +4,185 @@ function setup() {
 
 function draw() {
     background(46, 48, 64);
+    noStroke();
 
     // 1
     fill(13, 33, 191);
-    circle((width / 2.67), (height / 3), height / 3);
+    circle(325, 225, 250);
 
     // 2
     fill(0,0,0);
-    circle((width / 3), (height / 3.55), height / 5);
+    circle(300, 195, 175);
 
     // 3
     fill(251,230,85);
-    circle((width / 3), (height / 3.55), height / 5);
+    circle(90, 50, 25);
 
     // 4
     fill(249,153,216);
-    circle((width / 3), (height / 3.55), height / 5);
+    circle(110, 125, 50);
+
+    // 5 border border
+    fill(255,255,255);
+    circle(120, 350, 71);
+
+    // 5 border
+    fill(0,0,0);
+    circle(120, 350, 70);
 
     // 5
     fill(192,187,219);
-    circle((width / 3), (height / 3.55), height / 5);
+    circle(120, 350, 65);
 
     // 6
     fill(0,0,0);
-    circle((width / 3), (height / 3.55), height / 5);
+    circle(130, 338, 20);
 
     // 7
-    fill(203,140,221);
-    circle((width / 3), (height / 3.55), height / 5);
+    fill(203,140,221,200);
+    circle(375, 125, 70);
 
     // 8
-    fill(250,226,130);
-    circle((width / 3), (height / 3.55), height / 5);
+    fill(251,235,176);
+    circle(500, 100, 40);
 
     // 9
     fill(255,174,30);
-    circle((width / 3), (height / 3.55), height / 5);
+    circle(550, 135, 20);
 
-    // 10
-    fill(241,63,87);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 10
+    // fill(241,63,87);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 11
-    fill(72,133,126);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 11
+    // fill(72,133,126);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 12
-    fill(0,0,0);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 12
+    // fill(0,0,0);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 13
-    fill(107,191,219); // not sure here
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 13
+    // fill(107,191,219); // not sure here
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 14
-    fill(161,221,169);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 14
+    // fill(161,221,169);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 15
-    fill(255,165,53);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 15
+    // fill(255,165,53);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 16
-    fill(205,174,49);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 16
+    // fill(205,174,49);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 17
-    fill(244,115,171);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 17
+    // fill(244,115,171);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 18
-    fill(0,0,0);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 18
+    // fill(0,0,0);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 19
-    fill(81,187,226);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 19
+    // fill(81,187,226);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 20
-    fill(169,177,214);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 20
+    // fill(169,177,214);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 21
-    fill(0,0,0);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 21
+    // fill(0,0,0);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 22
-    fill(191,161,211);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 22
+    // fill(191,161,211);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 23
-    fill(213,101,137);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 23
+    // fill(213,101,137);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 24
-    fill(0,0,0);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 24
+    // fill(0,0,0);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 25
-    fill(213,101,137);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 25
+    // fill(213,101,137);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 26
-    fill(201,101,75);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 26
+    // fill(201,101,75);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 27
-    fill(155,95,71);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 27
+    // fill(155,95,71);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 28
-    fill(232,47,88);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 28
+    // fill(232,47,88);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 29
-    fill(247,229,101);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 29
+    // fill(247,229,101);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 30
-    fill(246,149,203);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 30
+    // fill(246,149,203);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 31
-    fill(51,164,222);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 31
+    // fill(51,164,222);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 32
-    fill(0,0,0);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 32
+    // fill(0,0,0);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 33
-    fill(247,182,52);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 33
+    // fill(247,182,52);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 34
-    fill(206,173,128);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 34
+    // fill(206,173,128);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 35
-    fill(133,212,217);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 35
+    // fill(133,212,217);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 36
-    fill(18,76,222);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 36
+    // fill(18,76,222);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 37
-    fill(200,35,51);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 37
+    // fill(200,35,51);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 38
-    fill(0,0,0);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 38
+    // fill(0,0,0);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 39
-    fill(209,184,83);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 39
+    // fill(209,184,83);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 40
-    fill(198,203,222);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 40
+    // fill(198,203,222);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 41
-    fill(240,222,18);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 41
+    // fill(240,222,18);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 42
-    fill(203,46,75);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 42
+    // fill(203,46,75);
+    // circle((width / 3), (height / 3.55), height / 5);
 
-    // 43
-    fill(60,139,182);
-    circle((width / 3), (height / 3.55), height / 5);
+    // // 43
+    // fill(60,139,182);
+    // circle((width / 3), (height / 3.55), height / 5);
 }
